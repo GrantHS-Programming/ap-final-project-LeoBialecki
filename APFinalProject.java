299da8aac0e5ed5d0313abf580baa7083720c226
lbialecki8259 <hi@gmail.com>
2024-05-09 11:39:09 -0700

    done for today

---
+++ APFinalProject.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class APFinalProject {
+    public static void main(String[] args){new APFinalProject();}
+
+    public APFinalProject(){
+        <a href="https://www.exchangerate-api.com">Rates By Exchange Rate API</a>
+        System.out.println("Welcome to Currency Converter!");
+        System.out.println();
+    }
+    Scanner myScanner = new Scanner(System.in);
+
+}
